a75befbaeccf22165bb910dabbfa708ba69ea503
Lukasz Kaminski <lukasznoroff@gmail.com>
2021-02-07 20:56:28 +0100

    first commit

---
+++ js/question-1.js
@@ -0,0 +1,57 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+
+const resultsContainer = document.querySelector(".results");
+const messageBox = document.querySelector(".message-box");
+const message = showMessage();
+
+async function makeApiCall() {
+    try {
+        const response = await fetch(url);
+
+        const result = await response.json();
+
+        const data = result.results;
+
+        messageBox.innerHTML = message;
+
+        resultsContainer.innerHTML = "";
+
+
+        for (let i = 0; i < data.length; i++) {
+        
+            let name = "Unknown";
+            if (data[i].name !== null || data[i].name !== undefined) {
+                name = data[i].name
+            }
+
+            let rating = "Unknown";
+            if (data[i].rating !== null || data[i].rating !== undefined) {
+                rating = data[i].rating
+            }
+
+            let tags = "Unknown";
+            if ((Object.keys(data[i].tags).length) !== null || (Object.keys(data[i].tags).length) !== undefined) {
+                tags = (Object.keys(data[i].tags).length)
+            }
+
+
+            if (i === 8) {
+                break;
+            }
+
+            resultsContainer.innerHTML += `<div class="result">name: ${name}<br>
+                                                               ratings: ${rating}<br>
+                                                               tags: ${tags}<br>
+                                           </div>`
+
+        }
+
+    } catch (error) {
+
+        messageBox.innerHTML = errorMessage()
+
+    }
+}
+
+makeApiCall();
